9a993c980ab6b1b7f859db806222089e939f0045
virat65 <virat1322420@gmail.com>
2025-06-03 13:42:31 +0530

    change package.json

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Weather-App/', // <-- This must match your repo name!
+  plugins: [react()],
+   base: '/Weather-App/', // <-- This must match your repo name!
 })
